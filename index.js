112a3b0378f715f88514cffb35bc97b6ab37b946
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-03-08 13:24:45 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const router = require('./router');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+io.on('connection', socket => {
+  console.log('New connection!!!');
+
+  socket.on('disconnect', () => {
+    console.log('User left');
+  });
+});
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
